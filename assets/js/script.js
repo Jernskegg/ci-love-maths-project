72710893c36c279da4bab344c7b408552c437232
Jernskegg <casper.hille@outlook.com>
2021-04-25 15:28:54 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,19 +10,28 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked submit");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition")
 })
 
 /**
  * the main game "loop", called when the scripts is first loaded
  *  and after the users's answers has been processed
  */
-function runGame(){
+function runGame(gameType){
     let num1 = Math.floor(Math.random() * 25) +1;
-    let num1 = Math.floor(Math.random() * 25) +1;
+    let num2 = Math.floor(Math.random() * 25) +1;
+
+    if (gameType === "addition"){
+        displayAddtionQuestion(num1 , num2);
+    } else {
+        alert(`unkown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer(){
@@ -41,8 +50,10 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAddtionQuestion(){
-
+function displayAddtionQuestion(operand1, operand2){
+    document.getElementById(`operand1`).textContent = operand1;
+    document.getElementById(`operand2`).textContent = operand2;
+    document.getElementById(`operator`).textContent = "+";
 }
 function displaySubtractQuestion(){
     
